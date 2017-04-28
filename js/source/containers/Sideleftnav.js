fcc7fa2f5411d5ca5064db91ad0b858566d55f44
wjm <1584824430@qq.com>
2017-04-28 22:04:05 +0800

    x
    x

--- js/source/containers/Sideleftnav.js
+++ js/source/containers/Sideleftnav.js
@@ -5,21 +5,22 @@ import Sideleft_item from '../components/Sideleft_item';
 class Sideleftnav extends React.Component{
 	render(){
 		return <nav id="sideleft">
-		<Sideleft_item item_value="生鲜水果"/>
-		<Sideleft_item item_value="进口零食"/>
-		<Sideleft_item item_value="进口酒饮"/>
-		<Sideleft_item item_value="奶制品  "/>
-		<Sideleft_item item_value="休闲零食"/>
-		<Sideleft_item item_value="饼干糖巧"/>
-		<Sideleft_item item_value="酒水饮料"/>
-		<Sideleft_item item_value="冲饮保健"/>
-		<Sideleft_item item_value="米面杂粮"/>
-		<Sideleft_item item_value="油品调味"/>
-		<Sideleft_item item_value="个人清洁"/>
-		<Sideleft_item item_value="美容护肤"/>
+		<Sideleft_item item_value="生鲜水果 "/>
+		<Sideleft_item item_value="进口零食 "/>
+		<Sideleft_item item_value="进口酒饮 "/>
+		<Sideleft_item item_value="奶制品   "/>
+		<Sideleft_item item_value="休闲零食 "/>
+		<Sideleft_item item_value="饼干糖巧 "/>
+		<Sideleft_item item_value="酒水饮料 "/>
+		<Sideleft_item item_value="冲饮保健 "/>
+		<Sideleft_item item_value="米面杂粮 "/>
+		<Sideleft_item item_value="油品调味 "/>
+		<Sideleft_item item_value="个人清洁 "/>
+		<Sideleft_item item_value="美容护肤 "/>
 		<div id="sideleft_erweima" >
 		<img src="./source/image/erweima.jpg"/>
 		</div>
+		<Sideleft_item item_value="在线咨询"/>
 		</nav> ;
 	}
 
